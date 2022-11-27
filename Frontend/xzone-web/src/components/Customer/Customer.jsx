5a8a46918f2c7e92ae52a67e94e3b6ef57c3cd51
Muhammad Hunzalah Shakeel <92629025+MuhammadHunzalahShakeel@users.noreply.github.com>
2022-11-28 03:04:15 +0500

    Modify Customer Care Page, Dropdown in Profile

--- Frontend/xzone-web/src/components/Customer/Customer.jsx
+++ Frontend/xzone-web/src/components/Customer/Customer.jsx
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import PropTypes  from 'prop-types'
 import Navmenu from '../Navmenu/Navmenu.jsx';
+import Chatbot from '../Chatbot/Chatbot.jsx';
 import Header from '../Header/Header.jsx';
 export default function Customer(props) {
   return (
@@ -15,7 +16,7 @@ export default function Customer(props) {
         <h3>{props.title}</h3>
         <input className='search' type="search" placeholder='search'/>
         <a href="something" class="button1">Add Customer</a>
-      
+        <Chatbot/>
       </div>
      
     </div>
